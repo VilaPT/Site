3313c8f7d2f38f63c41722583ec1b4a302baae2c
VilaPT <140882170+VilaPT@users.noreply.github.com>
2026-08-27 16:47:34 +0100

    Add preview config module

---
+++ fazja-preview/js/config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = 'https://tsvjulkvmrscgqajttra.supabase.co';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_NVFEYqGjxqBeHb99FwKWjw_nd6YIE1h';
+export const PUBLIC_APP_URL = 'https://vilapt.github.io/Site/fazja/';
